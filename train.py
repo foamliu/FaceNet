fa864705bf759dcd3320fee703963ba7e430bca3
Yang Liu <foamliu@yeah.net>
2018-08-07 11:51:26 +0800

    loss -> nan

--- train.py
+++ train.py
@@ -53,7 +53,8 @@ if __name__ == '__main__':
         if pretrained_path is not None:
             new_model.load_weights(pretrained_path)
 
-    new_model.compile(optimizer='adam', loss=triplet_loss)
+    adam = keras.optimizers.Adam(clipvalue=5.)
+    new_model.compile(optimizer=adam, loss=triplet_loss)
 
     print(new_model.summary())
 
