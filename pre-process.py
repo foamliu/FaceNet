72303d91288ff3e7a10c4d0815ac8fa7662ead62
Yang Liu <foamliu@yeah.net>
2018-08-07 10:12:03 +0800

    check data

--- pre-process.py
+++ pre-process.py
@@ -1,5 +1,6 @@
 import os
 import zipfile
+from multiprocessing import Pool
 
 import cv2 as cv
 import dlib
@@ -7,6 +8,8 @@ from tqdm import tqdm
 
 from config import identity_annot_filename, image_folder, img_size
 from utils import ensure_folder
+
+detector = dlib.get_frontal_face_detector()
 
 
 def extract(folder):
@@ -16,30 +19,37 @@ def extract(folder):
         zip_ref.extractall('data')
 
 
+def check_one_image(line):
+    line = line.strip()
+    if len(line) > 0:
+        tokens = line.split(' ')
+        image_name = tokens[0].strip()
+        id = tokens[1].strip()
+        filename = os.path.join(image_folder, image_name)
+        try:
+            image_bgr = cv.imread(filename)
+            image_rgb = cv.cvtColor(image_bgr, cv.COLOR_BGR2RGB)
+            dets = detector(image_rgb, 1)
+            if len(dets) > 0:
+                d = dets[0]
+                x1, y1, x2, y2 = d.left(), d.top(), d.right(), d.bottom()
+                image_rgb = image_rgb[y1:y2 + 1, x1:x2 + 1]
+            else:
+                print('cannot find face: ' + str(filename))
+            image_rgb = cv.resize(image_rgb, (img_size, img_size), cv.INTER_CUBIC)
+        except cv.error as err:
+            print(err)
+            print(filename)
+
+
 def check_image():
-    detector = dlib.get_frontal_face_detector()
     with open(identity_annot_filename, 'r') as file:
         lines = file.readlines()
-
-    for line in tqdm(lines):
-        line = line.strip()
-        if len(line) > 0:
-            tokens = line.split(' ')
-            image_name = tokens[0].strip()
-            id = tokens[1].strip()
-            filename = os.path.join(image_folder, image_name)
-            try:
-                image_bgr = cv.imread(filename)
-                image_rgb = cv.cvtColor(image_bgr, cv.COLOR_BGR2RGB)
-                dets = detector(image_rgb, 1)
-                if len(dets) > 0:
-                    d = dets[0]
-                    x1, y1, x2, y2 = d.left(), d.top(), d.right(), d.bottom()
-                    image_rgb = image_rgb[y1:y2 + 1, x1:x2 + 1]
-                image_rgb = cv.resize(image_rgb, (img_size, img_size), cv.INTER_CUBIC)
-            except cv.error as err:
-                print(err)
-                print(filename)
+    pool = Pool(24)
+    for _ in tqdm.tqdm(pool.imap_unordered(check_one_image, lines), total=len(lines)):
+        pass
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
