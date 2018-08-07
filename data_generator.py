69536c8720fec2e4b2a0befe067197dc79b73098
Yang Liu <foamliu@yeah.net>
2018-08-07 11:27:26 +0800

    add logs

--- data_generator.py
+++ data_generator.py
@@ -2,7 +2,6 @@
 import os
 
 import cv2 as cv
-import dlib
 import numpy as np
 from keras.applications.inception_resnet_v2 import preprocess_input
 from keras.utils import Sequence
@@ -19,8 +18,6 @@ class DataGenSequence(Sequence):
             self.samples = select_triplets(num_train_samples)
         else:
             self.samples = select_triplets(num_valid_samples)
-
-        self.detector = dlib.get_frontal_face_detector()
 
     def __len__(self):
         return int(np.ceil(len(self.samples) / float(batch_size)))
@@ -39,11 +36,6 @@ class DataGenSequence(Sequence):
                 filename = os.path.join(image_folder, image_name)
                 image_bgr = cv.imread(filename)
                 image_rgb = cv.cvtColor(image_bgr, cv.COLOR_BGR2RGB)
-                dets = self.detector(image_rgb, 1)
-                if len(dets) > 0:
-                    d = dets[0]
-                    x1, y1, x2, y2 = d.left(), d.top(), d.right(), d.bottom()
-                    image_rgb = image_rgb[y1:y2 + 1, x1:x2 + 1]
                 image_rgb = cv.resize(image_rgb, (img_size, img_size), cv.INTER_CUBIC)
                 batch_inputs[j, i_batch] = image_rgb
 
