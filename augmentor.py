a8424d74560d2ac3108074a7f0c0ee97e22324b6
Yang Liu <foamliu@yeah.net>
2018-08-30 16:53:09 +0800

    udpate

--- augmentor.py
+++ augmentor.py
@@ -13,9 +13,7 @@ aug_pipe = iaa.Sequential(
                    [
                        # sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
                        iaa.OneOf([
-                           iaa.GaussianBlur((0, 0.5)),  # blur images with a sigma between 0 and 3.0
-                           iaa.AverageBlur(k=(2, 3)),  # blur image using local means with kernel sizes between 2 and 3
-                           iaa.MedianBlur(k=(3, 5)),
+                           iaa.GaussianBlur((0, 0.5)),  # blur images with a sigma between 0 and 0.5
                            # blur image using local medians with kernel sizes between 2 and 7
                        ]),
                        iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen images
