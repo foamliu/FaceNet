07afbfc51396ade7ad97a05465f3836d188b384c
Yang Liu <foamliu@yeah.net>
2018-08-31 21:47:30 +0800

    update

--- train.py
+++ train.py
@@ -54,8 +54,9 @@ if __name__ == '__main__':
         if pretrained_path is not None:
             new_model.load_weights(pretrained_path)
 
-    sgd = keras.optimizers.SGD(lr=5e-6, momentum=0.9, nesterov=True, decay=1e-6)
-    new_model.compile(optimizer=sgd, loss=triplet_loss)
+    #sgd = keras.optimizers.SGD(lr=5e-6, momentum=0.9, nesterov=True, decay=1e-6)
+    adam = keras.optimizers.Adam(lr=0.05)
+    new_model.compile(optimizer=adam, loss=triplet_loss)
 
     print(new_model.summary())
 
