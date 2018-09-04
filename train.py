180dfd9fbe0b3d05b1ace32b9fea09a6c1c83cc9
Yang Liu <foamliu@yeah.net>
2018-09-05 07:19:53 +0800

    update

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
             new_model.load_weights(pretrained_path)
 
     sgd = keras.optimizers.SGD(lr=0.002, momentum=0.9, nesterov=True, decay=1e-6)
-    # adam = keras.optimizers.Adam(lr=0.002)
+    # adam = keras.optimizers.Adam(lr=0.001)
     new_model.compile(optimizer=sgd, loss=triplet_loss)
 
     print(new_model.summary())
